9623c2f276ce8ab9f968389d92cd6d27e7ad0972
robguz83 <robguz83@gmail.com>
2016-03-31 15:20:35 +0000

    limpieza

--- otro.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-touch ${1}.sh && chmod +x ${1}.sh && echo '#!/bin/bash' > ${1}.sh && echo '# -*- ENCODING: UTF-8 -*-' >> ${1}.sh
-# -*- ENCODING: UTF-8 -*-
