52a174cad80e8decb4b19b446f235e8fc5605ff2
robguz83 <robguz83@gmail.com>
2016-04-06 21:00:25 +0000

    Deploy Fecha_Creacion: 2016-04-06 21:00:25

--- _guardar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-
-rm $HOME/[a-zA-Z0-9]*.sh
-cp -a $HOME/workspace/*.sh $HOME/
-rm $HOME/workspace/*.sh
-
-exit
--- _nuevo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-
-touch $HOME/${1}.sh && chmod +x $HOME/${1}.sh && echo '#!/bin/bash' > $HOME/${1}.sh &&  echo '# -*- ENCODING: UTF-8 -*-' >> $HOME/${1}.sh && echo '' >> $HOME/${1}.sh && echo '' >> $HOME/${1}.sh && echo '' >> $HOME/${1}.sh && echo 'exit' >> $HOME/${1}.sh 
-chmod a+x $HOME/${1}.sh
-
-exit
